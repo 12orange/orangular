26f6c8bee72d7492c5f268b650b06bfb7c240a70
kwonjaelee <kwonjae13@gmail.com>
2018-03-29 01:18:32 +0900

    Add get users function

--- api/users/views.py
+++ api/users/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+from pymongo import MongoClient
 
 def index(request):
-    return HttpResponse("Hello, Django44444")
+    client = MongoClient('13.125.242.38', 27017)
+    db = client["12Orange"]
+    collection = db["users"]
+
+    posts = collection.find()
+    result = ", ".join(posts)
+
+    return HttpResponse(result)
